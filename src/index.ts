702fae6f7e8d9f6830691b00c664933ad648eb8f
Danilo <daniloassis.ti@gmail.com>
2022-05-27 17:37:24 -0300

    fix(serve): serve

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 const serve = require('koa-static');
 const Koa = require('koa');
+const mount = require('koa-mount');
 const app = new Koa();
 
-// $ GET /package.json
-app.use(serve('.'));
-
-// $ GET /hello.txt
-app.use(serve('static'));
-
-// or use absolute paths
-app.use(serve(__dirname + '/static'));
+app.use(mount('/public', serve(__dirname + '/static')));
 
 app.listen(3000);
-
 console.log('listening on port 3000');
